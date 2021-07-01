90c472b1e8a28188e69c62af6bfd8d8755d6a9e2
Jhon Jairo Moreno Trujillo <jhontrujillo12@gmail.com>
2021-07-01 17:04:54 -0500

    Update DemoApplication.java

--- src/main/java/com/jhm/demo/DemoApplication.java
+++ src/main/java/com/jhm/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Se crea otro comentario
+//Este metodo se encarga de hacer pruebas
 @SpringBootApplication
 public class DemoApplication {
 

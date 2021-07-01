00fb0b87c0051b8731d79b5e5eb04c1f4bc657fb
Jhon Jairo <jhonmore@bancolombia.com>
2021-07-01 16:05:38 -0500

    Se crea comentario de prueba

--- src/main/java/com/jhm/demo/DemoApplication.java
+++ src/main/java/com/jhm/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.jhm.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Se crea comentario de prueba
 @SpringBootApplication
 public class DemoApplication {
 

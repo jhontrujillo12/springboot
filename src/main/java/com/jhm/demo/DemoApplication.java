f70d6ec5ec08feeb91c88936568dbb8fd7444559
Jhon Jairo <jhonmore@bancolombia.com>
2021-07-01 17:05:55 -0500

    Se agregan comentarios grandes

--- src/main/java/com/jhm/demo/DemoApplication.java
+++ src/main/java/com/jhm/demo/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Se crea otro comentario
 @SpringBootApplication
+/**
+ * Hago este comentario grande para especificar funcionalidades
+ */
 public class DemoApplication {
 
 	public static void main(String[] args) {

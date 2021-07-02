469b127eb2978505bd07f22a5903eb3ff2d0efb2
Jhon Jairo <jhonmore@bancolombia.com>
2021-07-02 11:36:28 -0500

    Se coloca comentario en la rama principal

--- src/main/java/com/jhm/demo/DemoApplication.java
+++ src/main/java/com/jhm/demo/DemoApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-	    
+
+	    //Se agrega comentario de prueba
 		SpringApplication.run(DemoApplication.class, args);
 	}
 

8385768b9a86e55b5da23a2692035240ba9af502
Jhon Jairo <jhonmore@bancolombia.com>
2021-07-02 10:04:00 -0500

    Se eliminan comentarios

--- src/main/java/com/jhm/demo/DemoApplication.java
+++ src/main/java/com/jhm/demo/DemoApplication.java
@@ -4,24 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//Se crea otro comentario
-//Este metodo se encarga de hacer pruebas
 @SpringBootApplication
-/**
- * Hago este comentario grande para especificar funcionalidades
- */
 
-// Comentario para que muestre conflictos
-//Método encargado de hacer el demo de la aplicación
+
 public class DemoApplication {
 
-	//Método principal
+	
 	public static void main(String[] args) {
 	    	//Corre la aplicación
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	//Metodo para imprimir
+	
 	public void imprimir() {
 		System.out.println("Imprime en consola");
 	}
